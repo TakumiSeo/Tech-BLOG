118ce3b5912ba09cb0f5b86343237a8aae154669
takumiseo <takumiseo.microsoft.com>
2026-01-27 17:18:44 +0900

    fix publishconf

--- publishconf.py
+++ publishconf.py
@@ -4,6 +4,12 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(BASE_DIR))
+
 from pelicanconf import *  # noqa: F401,F403
 
 SITEURL = os.environ.get("SITEURL", "")
